9e0e163f067076d40b1faca55f7346b3a47fa89e
Sushant Barma <sushantbarma432@gmail.com>
2024-09-22 21:14:36 +0545

    Updated Abstract Classes.cs

--- Abstract Classes.cs
+++ Abstract Classes.cs
@@ -4,20 +4,22 @@ namespace Code
 {
     abstract class Professions
     {
-        public abstract string Name { get; set; }
-        public abstract string Position { get; set; }
-        public abstract string Experience_Years { get; set; }
+        public abstract string name { get; set; }
+        public abstract string pos { get; set; }
+        public abstract int expYrs { get; set; }
+        public abstract string mainLng { get; set; }
         public abstract void Info();
     }
 
     class Software_Developer: Professions
     {
-        public override string Name { get; set; }
-        public override string Position { get; set; }
-        public override string Experience_Years { get; set; }
+        public override string name { get; set; }
+        public override string pos { get; set; }
+        public override int expYrs { get; set; }
+        public override string mainLng { get; set; }
         public override void Info()
         {
-            Console.WriteLine($"\n--------------------------------------------------\nName: {this.Name}\nPosition: {this.Position}\nExperience Years: {this.Experience_Years}\n--------------------------------------------------\n");
+            Console.WriteLine($"\n--------------------------------------------------\nName: {this.name}\nPosition: {this.pos}\nExperience Years: {this.expYrs}\nMain Programming Language: {this.mainLng}\n--------------------------------------------------\n");
         }
     }
 
@@ -27,23 +29,26 @@ namespace Code
         {
             var software_dev1 = new Software_Developer
             {
-                Name = "Carl",
-                Position = "Junior Developer",
-                Experience_Years = "1 Year"
+                name = "Carl",
+                pos = "Junior Developer",
+                expYrs = 2,
+                mainLng = "Java"
             };
 
             var software_dev2 = new Software_Developer
             {
-                Name = "Jack",
-                Position = "Senior Developer",
-                Experience_Years = "3 Years"
+                name = "Jack",
+                pos = "Junior Developer",
+                expYrs = 2,
+                mainLng = "JavaScript"
             };
 
             var software_dev3 = new Software_Developer
             {
-                Name = "Jaiden",
-                Position = "Senior Developer",
-                Experience_Years = "5 Years"
+                name = "Jaiden",
+                pos = "Senior Developer",
+                expYrs = 5,
+                mainLng = "C++"
             };
 
             software_dev1.Info();
