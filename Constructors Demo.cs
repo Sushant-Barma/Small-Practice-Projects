49478d4e07fae2a95f25b0cab2cdc6a3fe60e1bd
Sushant Barma <sushantbarma432@gmail.com>
2024-09-23 20:06:55 +0545

    A file showing a simple example of how Constructors are implemented and used in C#

--- Constructors Demo.cs
+++ Constructors Demo.cs
@@ -2,16 +2,17 @@
 
 namespace Code
 {
+    //Declaing an Employee class
     class Employee
     {
-        public Employee(string name, int age, int id)
+        public Employee(string name, int age, int id) //Declaing our Constructor for this class
         {
-            this.name = name;
-            this.age = age;
-            this.id = id;
+            this.name = name; //Taking value for "name" string variable
+            this.age = age; //Taking value for "age" int variable
+            this.id = id; //Taking value for "id" int variable
         }
 
-        public void Info()
+        public void Info() //Declaring a method called "Info()" that displays the details of the asked employee
         {
             Console.WriteLine($"\n--------------------------------------------------\nEmployee's Details:\n\nName: {this.name}\nAge: {this.age}\nId: {this.id}\n--------------------------------------------------\n");
         }
@@ -21,18 +22,21 @@ namespace Code
     {
         static void Main(string[] args)
         {
+            //Initializing 5 Employee Objects with properties already assigned with the help of Constructors
             var emp1 = new Employee("Carl", 24, 432132);
             var emp2 = new Employee("Jack", 24, 432132);
             var emp3 = new Employee("Denis", 24, 432132);
             var emp4 = new Employee("Jaiden", 24, 432132);
             var emp5 = new Employee("Mark", 24, 432132);
 
+            //Displaying the details of the employees using "Info()" method declared inside "Employee" class
             emp1.Info();
             emp2.Info();
             emp3.Info();
             emp4.Info();
             emp5.Info();
 
+            //Waiting for user to press any key to continue...
             Console.ReadKey();
         }
     }
